94b067f98d434daf1039fcc50989443694a324bc
rk0907 <ravim.subs@gmail.com>
2024-05-31 10:59:25 -0500

    added annotations

--- go-mongo/cmd/main/main.go
+++ go-mongo/cmd/main/main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"my-go-projects/go-mongo/controllers"
+
 	"github.com/julienschmidt/httprouter"
-	"github.com/rkmangalp/go-mongo/controllers"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo/v2"
 )
 
 func main() {
 
+	// creates a new router
 	r := httprouter.New()
 
+	// Initialize the UserController with a MongoDB session
 	uc := controllers.NewUserController(getSession())
+
+	//creating routes
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
 
+	// Start the server
 	http.ListenAndServe("localhost:8080", r)
 
 }
 
 func getSession() *mgo.Session {
 
-	s, err := mgo.Dial("mongodb://localhost:27107")
+	// Connect to MongoDB on the default port
+	s, err := mgo.Dial("mongodb://localhost:27017")
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to connect to MongoDB: ", err)
 	}
 
 	return s
--- go-mongo/controllers/user.go
+++ go-mongo/controllers/user.go
@@ -5,61 +5,82 @@ import (
 	"fmt"
 	"net/http"
 
+	"my-go-projects/go-mongo/models"
+
 	"github.com/julienschmidt/httprouter"
-	"github.com/rkmangalp/my-go-projects/go-mongo/models"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type UserController struct {
+	// MongoDB session
 	session *mgo.Session
 }
 
 func NewUserController(s *mgo.Session) *UserController {
+
+	// Create a new UserController with the given session
 	return &UserController{s}
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
+	// Get the user ID from the route parameters
 	id := p.ByName("id")
 
+	// Check if the ID is a valid BSON ObjectID
 	if !bson.IsObjectIdHex(id) {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
-	oid := bson.ObjectHex(id)
+	// Convert the ID to a BSON ObjectId
+	oid := bson.ObjectIdHex(id)
 
+	// Create a User model instance
 	u := models.User{}
 
-	if err := uc.Session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
-		w.WritterHeader(404)
-		err
+	// Find the user by ID in the database
+	if err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		fmt.Println(err)
 	}
 
+	// Marshal the user to JSON
 	uj, err := json.Marshal(u)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Set the response content type to JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", uj)
 
 }
-func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Param) {
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
+	// Create a User model instance
 	u := models.User{}
 
+	// Decode the request body into the user model
 	json.NewDecoder(r.Body).Decode(&u)
 
+	// Generate a new BSON ObjectId for the user
 	u.Id = bson.NewObjectId()
 
-	uc.session.DB("mongo-golang").C("users").Insert(u)
+	// Insert the new user into the database
+	if err := uc.session.DB("mongo-golang").C("users").Insert(u); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 
+	// Marshal the user to JSON
 	uj, err := json.Marshal(u)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// Set the response content type to JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "%s\n", uj)
