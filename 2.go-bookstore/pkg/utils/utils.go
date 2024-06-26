0cf3418b098c87245caae758658af4c3a9c16abb
rk0907 <ravim.subs@gmail.com>
2024-06-26 13:33:38 -0500

    updated the main.go with annotations

--- 2.go-bookstore/pkg/utils/utils.go
+++ 2.go-bookstore/pkg/utils/utils.go
@@ -2,13 +2,13 @@ package utils
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
 // ParseBody parses the request body into the provided struct
 func ParseBody(r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
+	if body, err := io.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
 		}
