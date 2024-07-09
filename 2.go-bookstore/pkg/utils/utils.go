2b1dc528d26d1f420c799ad50f073c37ffa735f3
rk0907 <ravim.subs@gmail.com>
2024-07-09 13:55:18 -0500

    updated parse body

--- 2.go-bookstore/pkg/utils/utils.go
+++ 2.go-bookstore/pkg/utils/utils.go
@@ -9,7 +9,7 @@ import (
 // ParseBody parses the request body into the provided struct
 func ParseBody(r *http.Request, x interface{}) {
 	if body, err := io.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
+		if err := json.Unmarshal((body), x); err != nil {
 			return
 		}
 	}
